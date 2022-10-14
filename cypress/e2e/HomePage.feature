52cc1de88ec693dd49f6e22746385cad91e6df1b
Jose Bolanos <joseAlexis>
2022-10-14 16:59:21 +0200

    Migration to new cucumber preprocessor

--- cypress/e2e/HomePage.feature
+++ cypress/e2e/HomePage.feature
@@ -7,11 +7,8 @@ Feature: Home Page
         When I add a new element to the cart
         Then A pop up is properly displayed
 
-
     Scenario: Serach for an item
         Given I navigate to the shopping page 
-        When I serach for an item
-            | name  |
-            | Faded |
+        When I serach for the item 'Faded'
         And I click over the results
         Then Item details are displayed
--- cypress/e2e/HomePage/stepsDefinitions.ts
+++ cypress/e2e/HomePage/stepsDefinitions.ts
@@ -1,4 +1,4 @@
-import { And, Then, When } from "cypress-cucumber-preprocessor/steps"
+import {Given, Then, When } from "@badeball/cypress-cucumber-preprocessor"
 
 When('I add a new element to the cart', () => {
     cy.get('div.product-container:nth-child(1)').invoke('hover');
@@ -9,11 +9,11 @@ Then('A pop up is properly displayed', () => {
     cy.get('div#layer_cart').should('be.visible');
 });
 
-When('I serach for an item', (dataTable) => {
-    cy.get('input#search_query_top').type(dataTable.rawTable[1][0]);
+When('I serach for the item {string}', (itemName: string) => {
+    cy.get('input#search_query_top').type(itemName);
 });
 
-And('I click over the results', () => {
+When('I click over the results', () => {
     cy.get('div.ac_results > ul > li').as('result');
     cy.get('@result').should('be.visible');
     cy.get('@result').click();
